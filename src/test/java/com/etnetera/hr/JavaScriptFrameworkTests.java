4af03112229e39379a299ce94158caf47a22bb2c
Stefan Marcin <stefan.marcin.ext@uniqa.at>
2021-10-15 10:22:17 +0200

    [NOTICKET] Project inited.

---
+++ src/test/java/com/etnetera/hr/JavaScriptFrameworkTests.java
@@ -0,0 +1,17 @@
+package com.etnetera.hr;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Class used for Spring Boot/MVC based tests.
+ * 
+ * @author Etnetera
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaScriptFrameworkTests {
+
+}
